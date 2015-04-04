d3e5c8a8355611a7684dfb465e8ddb712799da49
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2015-04-04 00:44:33 -0400

    Basic content

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'TRB/ADC70'
+SITENAME = 'Transportation Energy Committee'
+SITEURL = ''
+
+PATH = 'content'
+STATIC_PATHS = ['pdf']
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'pelican-bootstrap3'
+DISPLAY_PAGES_ON_MENU = False
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
