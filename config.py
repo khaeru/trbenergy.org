0096cce338312ff31ea123ae754e15ce2ff0bc79
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2017-03-06 18:34:28 -0500

    dummy translation plugin to squash error in pelican-bootstrap3

--- config.py
+++ config.py
@@ -38,3 +38,36 @@ RELATIVE_URLS = True
 # Load the plugin for TRB committee members and presentations
 PLUGINS = [plugins.trb]
 TRB = private_settings.TRB
+
+
+# See
+# https://github.com/getpelican/pelican-themes/issues/460#issuecomment-269821765
+class i18n(object):
+    # looks for translations in
+    # {LOCALE_DIR}/{LANGUAGE}/LC_MESSAGES/{DOMAIN}.mo
+    # if not present, falls back to default
+
+    DOMAIN = 'messages'
+    LOCALE_DIR = '{THEME}/translations'
+    LANGUAGES = ['de']
+    NEWSTYLE = True
+
+    __name__ = 'i18n'
+
+    def register(self):
+        from pelican.signals import generator_init
+        generator_init.connect(self.install_translator)
+
+    def install_translator(self, generator):
+        import gettext
+        try:
+            translator = gettext.translation(
+                self.DOMAIN,
+                self.LOCALE_DIR.format(THEME=THEME),
+                self.LANGUAGES)
+        except (OSError, IOError):
+            translator = gettext.NullTranslations()
+        generator.env.install_gettext_translations(translator, self.NEWSTYLE)
+
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+PLUGINS.append(i18n())
